c7e9954ae821df8ca257e6b212c8c481f236f041
Bruno de Andrade Dinis <bruno.a.dinis96@gmail.com>
2023-04-12 22:03:42 -0300

    atualizações para a pagina de contato

--- assets/js/fomulario.js
+++ assets/js/fomulario.js
@@ -1,7 +1,58 @@
-function fomulario(){
-    let dados = [];
+// validação de inputs
+const inputs_contato = document.querySelectorAll('input[name=dados_contato]')
 
-    dados = document.querySelectorAll("input");
+const form = document.querySelector('form');
+const nomeInput = document.querySelector('#nome');
+const emailInput = document.querySelector('#email');
 
-    console.log(dados);
+form.addEventListener('submit', (event) => {
+  // Validar campo Nome
+  if (nomeInput.value.trim() === '') {
+    event.preventDefault(); // Cancela o envio do formulário
+    nomeInput.classList.add('input-container--invalido');
+    nomeInput.nextElementSibling.innerText = 'Por favor, preencha seu nome';
+  } else {
+    nomeInput.classList.remove('input-container--invalido');
+    nomeInput.nextElementSibling.innerText = '';
+  }
+
+  // Validar campo E-mail
+  if (emailInput.validity.typeMismatch) {
+    event.preventDefault(); // Cancela o envio do formulário
+    emailInput.classList.add('input-container--invalido');
+    emailInput.nextElementSibling.innerText = 'O email digitado não é válido.';
+  } else {
+    emailInput.classList.remove('input-container--invalido');
+    emailInput.nextElementSibling.innerText = '';
+  }
+});
+
+function formulario() {
+  let nome = document.getElementById("nome");
+  console.log(nome.value);
+
+  let email = document.getElementById("email");
+  console.log(email.value);
+
+  let whats = document.getElementById("whats");
+  console.log(whats.value);
+
+  const dados__pessoais = [nome.value, email.value, whats.value];
+  console.log(dados__pessoais);
+
+  const checkboxes = document.querySelectorAll('input[type=checkbox][name=dados-orcamento]:checked');
+  console.log(checkboxes.values)
+
+  let valores = [];
+
+  if (checkboxes.length > 0) {
+      checkboxes.forEach((checkbox) => {
+          valores.push(checkbox.value);
+      });
+  }
+
+
+  console.log(valores)
+
+  alert("Agora é só aguardar o nosso retorno em até 3 dias úteis!")
 }
